d2ae60c8ec8348ff6b22a3496d08abae9f0696d0
Jaroslaw Zola <jaroslaw.zola@gmail.com>
2012-09-03 16:11:48 -0500

    sync

--- src/mpi/sketch/validate_edges.hpp
+++ src/mpi/sketch/validate_edges.hpp
@@ -41,7 +41,7 @@ inline std::pair<bool, std::string> validate_edges(const AppConfig& opt, AppLog&
 
     // divide into remote and local
     unsigned int last =
-	(std::partition(edges.begin(), edges.end(), not_local(seqs.front().id, seqs.back().id)) - edges.begin());
+	std::partition(edges.begin(), edges.end(), not_local(seqs.front().id, seqs.back().id)) - edges.begin();
 
     // local edges are easy :-)
     report << info << "processing local edges..." << std::endl;
@@ -53,7 +53,7 @@ inline std::pair<bool, std::string> validate_edges(const AppConfig& opt, AppLog&
     report << info << "processing remaining edges: " << std::flush;
 
     id2rank i2r(n, size);
-    unsigned int step = (static_cast<double>(last) / 10) + 0.5;
+    unsigned int step = (static_cast<double>(last) / 10) + 0.495;
 
     // sort according to location
     read2rank r2r(rank, i2r);
@@ -73,14 +73,18 @@ inline std::pair<bool, std::string> validate_edges(const AppConfig& opt, AppLog&
 
 	for (unsigned int i = 1; i < last + 1; ++i) {
 	    int crank = -1;
+	    if (i < last) crank = rank_id[i].first;
 	    if (rank_id[pos].first != crank) {
 		range.push_back(std::make_pair(pos, i));
 		pos = i;
 	    }
 	} // for i
 
+	std::srand(std::time(0));
+	std::random_shuffle(range.begin(), range.end());
+
 	// process blocks
-	step = (static_cast<double>(range.size()) / 10) + 0.5;
+	step = (static_cast<double>(range.size()) / 10) + 0.495;
 	std::vector<std::string> sv;
 
 	for (unsigned int i = 0; i < range.size(); ++i) {
@@ -88,8 +92,8 @@ inline std::pair<bool, std::string> validate_edges(const AppConfig& opt, AppLog&
 	    rma_seq.get(rank_id.begin() + range[i].first, rank_id.begin() + range[i].second, sv);
 
 	    for (unsigned int j = range[i].first; j < range[i].second; ++j) {
-		if (i2r(edges[j].id0) == rank) edges[j] = ident(edges[j], sv[j - pos]);
-		else edges[j] = ident(sv[j - pos], edges[j]);
+		if (i2r(edges[j].id0) == rank) edges[j] = ident(edges[j], sv[j - range[i].first]);
+		else edges[j] = ident(sv[j - range[i].first], edges[j]);
 	    } // for j
 
 	} // for i
